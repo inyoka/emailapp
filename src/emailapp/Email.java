7a1dcc36af26eac1f915412f130ef62abea3224c
Simon <simonmarkholland@gmail.com>
2019-11-22 12:16:04 +0000

    Add method to request department.

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,6 @@
 package emailapp;
 
+import java.util.Scanner;
 
 public class Email {
 	private String firstName;
@@ -19,6 +20,17 @@ public class Email {
 //		Call method asking for department
 		this.department = setDepartment();
 		System.out.println("Department : " + this.department);
+	}
+	
+//	Ask for department
+	private String setDepartment() {
+		System.out.println("DEPARTMENT CODES\nEnter the department\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter Department Codes");
+		Scanner in = new Scanner(System.in);
+		int depChoice = in.nextInt();
+		if (depChoice == 1) { return "sales";}
+		else if (depChoice ==2 ) {return "dev";}
+		else if (depChoice ==3 ) {return "acct";}
+		else { return ""; }
 	}
 	
 //	Generate a random password
