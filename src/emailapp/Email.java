f9cc533c3272c099432f6edca090915e5f59b114
Simon <simonmarkholland@gmail.com>
2019-11-22 12:14:49 +0000

    Class skeleton

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -2,4 +2,19 @@ package emailapp;
 
 public class Email {
 
+public class Email {
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String department;
+	private int mailboxCapacity;
+	private String alternateEmail; 
+	
+//	Generate a random password
+	
+//	Set the mailbox capacity
+	
+//	Set the alternate email
+	
+//	Change the password
 }
